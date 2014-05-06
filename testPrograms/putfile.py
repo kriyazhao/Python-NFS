848ba487fff7c3183173dfaae1c73f48e372b750
kriyazhao <kriyazhao@users.noreply.github.com>
2014-05-05 21:22:49 -0400

    Create putfile.py

---
+++ testPrograms/putfile.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# putfile.py
+# A client-side request to put a file to the server
+# Ting Zhao (t.zhao2011@gmail.com)
+
+#==================================================================================================================
+# imports
+import requests
+import urllib2
+import argparse
+import hashlib
+
+#==================================================================================================================
+# main function
+def main():
+    
+	# parse the command line arguments using argparse module
+    argparser = argparse.ArgumentParser(description = "A client-side request to put a file to the server")
+    argparser.add_argument('--host', help = "host ip address")
+    argparser.add_argument('--port', type=int, help = "host port")
+    argparser.add_argument('--filename', help = "file to send")
+    args = argparser.parse_args()
+
+    # build the URL from the file content and request data
+    file = open(args.filename, 'rb').read()
+
+    hashMD5 = hashlib.md5(file).hexdigest()
+    hashSHA1 = hashlib.sha1(file).hexdigest()
+
+    url = "http://{0}:{1}/data/{2}/{3}" % (args.host, args.port, hashMD5, hashSHA1)
+    headers = {'Content-Type': 'application/octet-stream'}
+    print "MD5:  {0}\nSHA1: {1}".format(hashMD5, hashSHA1)
+    r = requests.put(url, data=file, headers=headers)
+    print r.text
+
+if __name__ == "__main__":
+    main()
