c50825d63f897d1b8b38ed115a74f476fc13727b
kriyazhao <kriyazhao@users.noreply.github.com>
2014-05-18 23:45:13 -0400

    Update deletefile.py

--- testPrograms/deletefile.py
+++ testPrograms/deletefile.py
@@ -9,6 +9,7 @@
 import requests
 import argparse
 import hashlib
+import json
 
 #==================================================================================================================
 # main function
@@ -20,14 +21,27 @@ def main():
     parseArg.add_argument('--port', type=int, help = "host port")
     parseArg.add_argument('--md5', help = "md5 code to send")
     parseArg.add_argument('--sha1', help = "sha1 code to send")
+    parseArg.add_argument('--username', help = "username to send")
+    parseArg.add_argument('--password', help = "password code to send")	
     args = parseArg.parse_args()
     
+    # login
+    url1 = "http://{0}:{1}/login".format(args.host, args.port)
+    hashPwd = hashlib.md5(args.password).hexdigest()
+    payload = json.dumps({"username":args.username, "password":hashPwd})
+    s = requests.post(url1, data = payload)	
+    print s.text
+	
     # build the URL from the file content and request data
-    url = "http://{0}:{1}/data/{2}/{3}".format(args.host, args.port, args.md5, args.sha1)
+    url2 = "http://{0}:{1}/data/{2}/{3}".format(args.host, args.port, args.md5, args.sha1)
     print "Requested file:"
     print "MD5:  {0}\n  SHA1: {1}".format(args.md5, args.sha1)
-    r = requests.delete(url)
-    print "Result: {0}".format(r.content)
-
+    r = requests.delete(url2)
+    print "Result: {0}".format(r.text)
+	
+	# logout
+    url3 = "http://{0}:{1}/logout".format(args.host, args.port)
+    o = requests.get(url3)
+    print o.text
 if __name__ == "__main__":
     main()
