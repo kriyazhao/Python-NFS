f335a60fcbab6652130927ec7dc59b16ab1c3df7
kriyazhao <kriyazhao@users.noreply.github.com>
2014-05-18 23:49:10 -0400

    Update getfile.py

--- testPrograms/getfile.py
+++ testPrograms/getfile.py
@@ -20,20 +20,28 @@ def main():
     parseArg.add_argument('--port', type=int, help = "host port")
     parseArg.add_argument('--md5', help = "md5 code to send")
     parseArg.add_argument('--sha1', help = "sha1 code to send")
+	parseArg.add_argument('--username', help = "username to send")
+    parseArg.add_argument('--password', help = "password code to send")	
     args = parseArg.parse_args()
     
+	# login
+    url1 = "http://{0}:{1}/login".format(args.host, args.port)
+    hashPwd = hashlib.md5(args.password).hexdigest()
+    payload = json.dumps({"username":args.username, "password":hashPwd})
+    s = requests.post(url1, data = payload)	
+    print s.text
+	
     # build the URL from the file content and request data
-    url = "http://{0}:{1}/data/{2}/{3}".format(args.host, args.port, args.md5, args.sha1)
+    url2 = "http://{0}:{1}/data/{2}/{3}".format(args.host, args.port, args.md5, args.sha1)
     print "Requested file:"
     print "MD5:  {0}\n  SHA1: {1}".format(args.md5, args.sha1)
-    r = requests.get(url)
-    result = r.content
-
-    hashMD5 = hashlib.md5(result).hexdigest()
-    hashSHA1 = hashlib.sha1(result).hexdigest()
-    print "Returned file:"
-    print "MD5:  {0}\n  SHA1: {1}".format(hashMD5, hashSHA1)
-	print result
+    r = requests.get(url2)
+    file = r.content
+	
+	# logout
+    url3 = "http://{0}:{1}/logout".format(args.host, args.port)
+    o = requests.get(url3)
+    print o.text
 
 if __name__ == "__main__":
     main()
