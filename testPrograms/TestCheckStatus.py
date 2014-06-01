d4ef7f055c67a0596cd9b9329af620478fb40408
kriyazhao <kriyazhao@users.noreply.github.com>
2014-06-01 19:33:56 -0400

    Create TestCheckStatus.py

---
+++ testPrograms/TestCheckStatus.py
@@ -0,0 +1,27 @@
+import unittest
+import requests
+import json
+import CheckLogin
+
+class TestCheckStatus(unittest.TestCase):
+    global session
+    
+    def test_login_fail(self):
+        session.logged_in = False
+        response = requests.get("/checklogin")
+        self.assertEqual(response.r, 0)
+        
+    def test_login_success(self):
+        session.logged_in = True
+        session.username = "kriyazhao"
+        response = requests.get("/checklogin")
+        self.assertEqual(response.r, 1)
+    
+    def test_login_success_return_username(self):
+        session.logged_in = True
+        session.username = "kriyazhao"
+        response = requests.get("/checklogin")
+        self.assertEqual(response.username, "kriyazhao")
+    
+if __name__ == "__main__":
+    unitest.main()
