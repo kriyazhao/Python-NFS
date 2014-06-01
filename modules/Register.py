6d4e92ebe2577723e86e8ae8bf9b0a1fb22d7670
kriyazhao <kriyazhao@users.noreply.github.com>
2014-06-01 16:24:50 -0400

    Update Register.py

--- modules/Register.py
+++ modules/Register.py
@@ -1,14 +1,3 @@
-#!/usr/bin/python
-#
-# -------------------------------------------------------------------------------------------------
-# DFS_daemon.py
-# Created on: 2014-03-08 22:30:50.00000
-# Updated on: 2014-05-28 21:19:34.00000
-# Author: Ting Zhao (t.zhao2011@gmail.com)
-# Description: A daemon program for a simple Distributed File System. 
-#              This is for handling requests (put, get, post and delete) from client to server
-# -------------------------------------------------------------------------------------------------
-
 #==========================================================================================================================
 # import python modules
 import logging, argparse, ConfigParser
