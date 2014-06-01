4753b2cc32023a540e1118192bb23c4ff9c3dfe7
kriyazhao <kriyazhao@users.noreply.github.com>
2014-06-01 18:27:28 -0400

    Update Register.py

--- modules/Register.py
+++ modules/Register.py
@@ -17,16 +17,19 @@ class Register:
         requestContent = json.loads(web.data())
         logging.info('username: ' + requestContent['un'])
         logging.info('hashpw: ' + requestContent['pw'])
-        if session.logged_in == False:
-            results = db.query("SELECT * FROM user WHERE username=$username", 
-                      vars={'username':requestContent['un']})
-            rows = list(results)
-            if len(rows) == 0:
-                db.insert("user", username=requestContent['un'], badge=requestContent['pw'], createon= str(datetime.datetime.now()))
-                session.logged_in = True
-                session.username = requestContent['un']
-                return json.dumps({'r':1,'un':session.username})
+        if requestContent['un'] == "" or requestContent['pw'] == "d41d8cd98f00b204e9800998ecf8427e":
+            if session.logged_in == False:
+                results = db.query("SELECT * FROM user WHERE username=$username", 
+                          vars={'username':requestContent['un']})
+                rows = list(results)
+                if len(rows) == 0:
+                    db.insert("user", username=requestContent['un'], badge=requestContent['pw'], createon= str(datetime.datetime.now()))
+                    session.logged_in = True
+                    session.username = requestContent['un']
+                    return json.dumps({'r':1,'un':session.username})
+                else:
+                    return json.dumps({'r':0})
             else:
-                return json.dumps({'r':0})
+                return json.dumps({'r':2,'un':session.username})
         else:
-            return json.dumps({'r':2,'un':session.username})
+            return json.dumps({'r':3})
