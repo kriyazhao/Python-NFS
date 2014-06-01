78db5a1e9b8cee49c7992ede4e87df1beda838c1
kriyazhao <kriyazhao@users.noreply.github.com>
2014-06-01 19:04:13 -0400

    Update TestRegister.py

--- testPrograms/TestRegister.py
+++ testPrograms/TestRegister.py
@@ -6,47 +6,40 @@ import Register
 class TestRegister(unittest.TestCase):
     global session
     
-    def test_username_empty_fail():
-        hashPwd = hashlib.md5("123").hexdigest()
-        dataload = json.dumps({"un": "", "pw": hashPwd})
-        response = requests.post("/register", dataload)
+    def test_username_empty_fail(self):
+        response = self.prepare_dataload_and_send_request("","123")
         self.assertEqual(response.r, 3)
         
-    def test_password_empty_fail():
-        hashPwd = hashlib.md5("").hexdigest()
-        dataload = json.dumps({"un": "123", "pw": hashPwd})
-        response = requests.post("/register", dataload)
+    def test_password_empty_fail(self):
+        response = self.prepare_dataload_and_send_request("123","")
         self.assertEqual(response.r, 3)
         
-    def test_input_valid_success():
-        hashPwd = hashlib.md5("abcd").hexdigest()
-        dataload = json.dumps({"un": "qwer", "pw": hashPwd})
-        response = requests.post("/register", dataload)
+    def test_input_valid_success(self):
+        response = self.prepare_dataload_and_send_request("qwer","abcd")
         self.assertNotEqual(response.r, 3)
 
-    def test_already_register_fail():
-        hashPwd = hashlib.md5("12345").hexdigest()
-        dataload = json.dumps({"un": "kriyazhao", "pw": hashPwd})
-        response = requests.post("/register", dataload)
+    def test_already_register_fail(self):
+        response = self.prepare_dataload_and_send_request("kriyazhao","12345")
         self.assertNotEqual(response.r, 0)
         
-    def test_new_register_success():
-        hashPwd = hashlib.md5("yygjk").hexdigest()
-        dataload = json.dumps({"un": "kriya", "pw": hashPwd})
-        response = requests.post("/register", dataload)
+    def test_new_register_success(self):
+        response = self.prepare_dataload_and_send_request("kriya","yygjk")
         session.logged_in == False
         session.kill()
         self.assertEqual(response.r, 1)
         
-    def test_already_login_fail():
+    def test_already_login_fail(self):
         session.logged_in = True
         session.username = "kriyazhao"
-        hashPwd = hashlib.md5("yygjk").hexdigest()
-        dataload = json.dumps({"un": "kriya", "pw": hashPwd})
-        response = requests.post("/register", dataload)
+        response = self.prepare_dataload_and_send_request("kriya","yygjk")
         session.logged_in == False
         session.kill()
         self.assertEqual(response.r, 2)
 
+    def prepare_dataload_and_send_request(self,un,pw):
+        hashPwd = hashlib.md5(pw).hexdigest()
+        dataload = json.dumps({"un": un, "pw": hashPwd})
+        return requests.post("/register", dataload)
+    
 if __name__ == "__main__":
     unitest.main()
