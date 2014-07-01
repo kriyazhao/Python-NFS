410276d1b534ac12b30bf7fc8120ba32875045c1
kriyazhao <kriyazhao@users.noreply.github.com>
2014-06-30 22:47:34 -0400

    Update TestUsernameValidator.py

--- NFS_Django/testCases/util/TestUsernameValidator.py
+++ NFS_Django/testCases/util/TestUsernameValidator.py
@@ -1,37 +1,37 @@
 import unittest
-import requests
 import json
-import Register
+import sys
+sys.path.append("/var/www/fs/NFS_Django/")
 from NFS_Django.views.util.UserValidator import UserValidator
 
 class TestUsernameValidator(unittest.TestCase):
 
-    def setUp(self):
-        print "TestUsernameValidator begins now......"
-        validator = UserValidator()
+    def setUp(self): 
+        print self._testMethodName + " begins now......"
+        self.validator = UserValidator()
     
     def test_username_empty_fail(self):
-        validator.setUsername("")
-        self.assertTrue(validator.usernameValidator())
+        self.validator.setUsername("")
+        self.assertFalse(self.validator.usernameValidate())
         
     def test_username_onlynum_fail(self):
-        validator.setUsername("12345")
-        self.assertTrue(validator.usernameValidator())
+        self.validator.setUsername("12345")
+        self.assertFalse(self.validator.usernameValidate())
 
     def test_username_startsnum_fail(self):
-        validator.setUsername("12345ting")
-        self.assertTrue(validator.usernameValidator())
+        self.validator.setUsername("12345ting")
+        self.assertFalse(self.validator.usernameValidate())
 
     def test_username_short_fail(self):
-        validator.setUsername("ti")
-        self.assertTrue(validator.usernameValidator())
+        self.validator.setUsername("ti")
+        self.assertFalse(self.validator.usernameValidate())
         
     def test_username_valid_success(self):
-        validator.setUsername("ting123")
-        self.assertTrue(validator.usernameValidator())
+        self.validator.setUsername("ting123")
+        self.assertTrue(self.validator.usernameValidate())
   
     def tearDown(self):
-        del validator
+        del self.validator
     
 if __name__ == "__main__":
-    unitest.main()
+    unittest.main()
